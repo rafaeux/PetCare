cf7e54861d8b01a9c3c574952b08cd88fc9809a3
Rafael Cunha <contatorafaeux@gmail.com>
2022-03-16 19:30:13 -0300

    project created

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, SafeAreaView, StatusBar } from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <StatusBar hidden={true} />
+    </SafeAreaView>
+  );
+};
+
+export default App;
