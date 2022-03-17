5e2cf0165f100da84c5cad824c29f86ba125480b
Rafael Cunha <contatorafaeloliveiradacunha@gmail.com>
2022-03-17 17:56:52 -0300

    feat(structure): structural part created

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
 import React from 'react';
-import { View, Text, SafeAreaView, StatusBar } from 'react-native';
+import { View, Text, SafeAreaView, StatusBar, StyleSheet } from 'react-native';
+
+import Header from './src/components/Header';
+import Body from './src/components/Body';
+import Footer from './src/components/Footer';
 
 const App = () => {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.container}>
       <StatusBar hidden={true} />
+      <Header />
+      { //<Body />
+        //<Footer />
+      }
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#c6d4e1'
+  }
+});
 
 export default App;
