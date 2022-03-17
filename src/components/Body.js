15f9dbc261df94773cdaa34283a421f4e09719ab
Rafael Cunha <contatorafaeloliveiradacunha@gmail.com>
2022-03-17 19:22:36 -0300

    fix(body structure): touchable opacity added

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,52 +1,21 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
 const Product = (props) => {
     return (
-        <View style={styles.product}>
-            <Text>{props.name}</Text>
-            <Text>{props.description}</Text>
-            <Text>{props.price}</Text>
-        </View>
+        <TouchableOpacity style={styles.product}>
+            <Text style={styles.productName}>{props.name}</Text>
+            <Text style={styles.productDescription}>{props.description}</Text>
+            <TouchableOpacity style={styles.productPrice}>
+                <Text style={styles.productPriceText}>{props.price}</Text>
+            </TouchableOpacity>
+        </TouchableOpacity>
     );
 }
 
 const Body = () => {
     return (
         <View style={styles.body}>
-            <Product name="Teste" description="Este é um produto teste que ainda não está para venda" price="R$ 25,00" />
-            <Product name="Teste" description="Este é um produto teste que ainda não está para venda" price="R$ 25,00" />
-            <Product name="Teste" description="Este é um produto teste que ainda não está para venda" price="R$ 25,00" />
-            <Product name="Teste" description="Este é um produto teste que ainda não está para venda" price="R$ 25,00" />
-            <Product name="Teste" description="Este é um produto teste que ainda não está para venda" price="R$ 25,00" />
-            <Product name="Teste" description="Este é um produto teste que ainda não está para venda" price="R$ 25,00" />
-            <Product name="Teste" description="Este é um produto teste que ainda não está para venda" price="R$ 25,00" />
-            <Product name="Teste" description="Este é um produto teste que ainda não está para venda" price="R$ 25,00" />
-            <Product name="Teste" description="Este é um produto teste que ainda não está para venda" price="R$ 25,00" />
-            <Product name="Teste" description="Este é um produto teste que ainda não está para venda" price="R$ 25,00" />
-            <Product name="Teste" description="Este é um produto teste que ainda não está para venda" price="R$ 25,00" />
-            <Product name="Teste" description="Este é um produto teste que ainda não está para venda" price="R$ 25,00" />
-            <Product name="Teste" description="Este é um produto teste que ainda não está para venda" price="R$ 25,00" />
-            <Product name="Teste" description="Este é um produto teste que ainda não está para venda" price="R$ 25,00" />
-            <Product name="Teste" description="Este é um produto teste que ainda não está para venda" price="R$ 25,00" />
-            <Product name="Teste" description="Este é um produto teste que ainda não está para venda" price="R$ 25,00" />
-            <Product name="Teste" description="Este é um produto teste que ainda não está para venda" price="R$ 25,00" />
-            <Product name="Teste" description="Este é um produto teste que ainda não está para venda" price="R$ 25,00" />
-            <Product name="Teste" description="Este é um produto teste que ainda não está para venda" price="R$ 25,00" />
-            <Product name="Teste" description="Este é um produto teste que ainda não está para venda" price="R$ 25,00" />
-            <Product name="Teste" description="Este é um produto teste que ainda não está para venda" price="R$ 25,00" />
-            <Product name="Teste" description="Este é um produto teste que ainda não está para venda" price="R$ 25,00" />
-            <Product name="Teste" description="Este é um produto teste que ainda não está para venda" price="R$ 25,00" />
-            <Product name="Teste" description="Este é um produto teste que ainda não está para venda" price="R$ 25,00" />
-            <Product name="Teste" description="Este é um produto teste que ainda não está para venda" price="R$ 25,00" />
-            <Product name="Teste" description="Este é um produto teste que ainda não está para venda" price="R$ 25,00" />
-            <Product name="Teste" description="Este é um produto teste que ainda não está para venda" price="R$ 25,00" />
-            <Product name="Teste" description="Este é um produto teste que ainda não está para venda" price="R$ 25,00" />
-            <Product name="Teste" description="Este é um produto teste que ainda não está para venda" price="R$ 25,00" />
-            <Product name="Teste" description="Este é um produto teste que ainda não está para venda" price="R$ 25,00" />
-            <Product name="Teste" description="Este é um produto teste que ainda não está para venda" price="R$ 25,00" />
-            <Product name="Teste" description="Este é um produto teste que ainda não está para venda" price="R$ 25,00" />
-            <Product name="Teste" description="Este é um produto teste que ainda não está para venda" price="R$ 25,00" />
             <Product name="Teste" description="Este é um produto teste que ainda não está para venda" price="R$ 25,00" />
             <Product name="Teste" description="Este é um produto teste que ainda não está para venda" price="R$ 25,00" />
         </View>
@@ -62,9 +31,32 @@ const styles = StyleSheet.create({
     },
     product: {
         backgroundColor: '#ffffff',
-        padding: 10,
+        padding: 15,
         elevation: 3,
-        marginBottom: 20
+        marginBottom: 20,
+        height: 150
+    },
+    productName: {
+        flex: 1,
+        fontSize: 24,
+        fontWeight: 'bold',
+    },
+    productDescription: {
+        flex: 1,
+        fontSize: 16,
+        color: '#B5B5B5'
+    },
+    productPrice: {
+        flex: 1,
+        backgroundColor: '#bdb8ad',
+        width: '20%',
+        justifyContent: 'center',
+        elevation: 3
+    },
+    productPriceText: {
+        color: '#ffffff',
+        fontWeight: 'bold',
+        textAlign: 'center',
     }
 });
 
