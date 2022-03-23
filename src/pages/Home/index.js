73cfffad3607af72c60ccb375a616e723464696b
Rafael Cunha <contatorafaeloliveiradacunha@gmail.com>
2022-03-23 08:04:18 -0300

    feat(footer): files and references removed

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,7 +3,6 @@ import { SafeAreaView, StatusBar, ScrollView, StyleSheet } from 'react-native';
 
 import Header from '../../components/Header';
 import Body from '../../components/Body';
-import Footer from '../../components/Footer';
 
 const Home = () => {
     return (
