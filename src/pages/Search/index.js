297cb6d1f1c0d75e409124f7ad767e90161be07c
Rafael Cunha <contatorafaeloliveiradacunha@gmail.com>
2022-03-27 19:43:44 -0300

    fix(search): body updated

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { SafeAreaView, StatusBar, ScrollView, StyleSheet } from 'react-native';
 
 import Header from '../../components/Header';
-import Body from './components/Body';
+import Body from '../../components/Body';
 
 const Search = () => {
     return (
