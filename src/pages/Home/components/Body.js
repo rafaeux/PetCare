88d91d50e6fda2dee29ebfa9c7b1f57355d440ac
Rafael Cunha <contatorafaeloliveiradacunha@gmail.com>
2022-03-28 19:33:18 -0300

    fix(home): body updated

--- src/pages/Home/components/Body.js
+++ src/pages/Home/components/Body.js
@@ -7,6 +7,9 @@ const Body = () => {
             <View style={styles.body}>
                 <Text style={styles.title}>Seja bem-vindo ao nosso aplicativo!</Text>
                 <Text style={styles.description}>Aqui você poderá controlar os gastos e os produtos que irá utilizar para os seus animais.</Text>
+                <Text style={styles.description}>Quem tem animal de estimação é apaixonado pelo seu amiguinho, pois eles são companheiros, doces e muito divertidos.</Text>
+                <Text style={styles.description}>Independentemente de qual seja o seu bichinho, um cachorro, um gato, calopsita dentre tantos outros, cuidar bem deles é essencial.</Text>
+                <Text style={styles.finalDescription}>E por isto, aqui está um jeito fácil que preparamos para você cuidar do seu pet sem surpresas no final do mês!</Text>
             </View>
         </View>
     );
@@ -20,7 +23,7 @@ const styles = StyleSheet.create({
         backgroundColor: '#7a1745'
     },
     body: {
-        height: 200,
+        height: 600,
         padding: 20,
         justifyContent: 'space-around',
         alignItems: 'center'
@@ -34,6 +37,11 @@ const styles = StyleSheet.create({
     description: {
         color: '#fcf4b6',
         fontSize: 18
+    },
+    finalDescription: {
+        color: '#ffffff',
+        fontSize: 24,
+        fontWeight: 'bold'
     }
 });
 
